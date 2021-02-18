ded322d6c614e790339e7f3aed70ac50eb8ad941
Pete Correia <pete@petecorreia.com>
2021-02-18 22:51:07 +0000

    Update

--- config.fish
+++ config.fish
@@ -16,26 +16,6 @@ alias sudo "sudo "
 
 # add ssh keys
 ssh-add -K ~/.ssh/id_rsa > /dev/null 2>&1
-ssh-add -K ~/.ssh/id_rsa_appital > /dev/null 2>&1
-
-# node -v
-
-# tabtab source for serverless package
-# uninstall by removing these lines or running `tabtab uninstall serverless`
-[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
-# tabtab source for sls package
-# uninstall by removing these lines or running `tabtab uninstall sls`
-[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
-# tabtab source for slss package
-# uninstall by removing these lines or running `tabtab uninstall slss`
-[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/petecorreia/google-cloud-sdk/path.fish.inc' ]; . '/Users/petecorreia/google-cloud-sdk/path.fish.inc'; end
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /usr/local/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
 
 # Docker
 export DOCKER_BUILDKIT=1
@@ -51,3 +31,6 @@ set -U fish_user_paths $GOPATH/bin $fish_user_paths
 set -g fish_user_paths "/Users/petecorreia/workspace/istio-1.6.1/bin" $fish_user_paths
 set -g fish_user_paths "/Users/petecorreia/workspace/sonar-scanner-4.4.0.2170-macosx/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/libpq/bin" $fish_user_paths
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/petecorreia/google-cloud-sdk/path.fish.inc' ]; . '/Users/petecorreia/google-cloud-sdk/path.fish.inc'; end
